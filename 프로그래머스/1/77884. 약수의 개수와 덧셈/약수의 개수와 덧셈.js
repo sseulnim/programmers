806199dabbac4f0d45f1adc92a327c04314a958d
Sseul <165990284+sseulnim@users.noreply.github.com>
2024-06-27 15:10:52 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for (var i = left; i <= right; i++) {
+        if (Math.sqrt(i) % 1 === 0) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
